7c0a0dc8ede71a0f04b0e45f15c920597023b63c
sadiq-1698 <mohammedsadiq1698@gmail.com>
2024-06-08 14:11:57 +0530

    cleanup: rewrite logo and link components

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,4 +1,5 @@
-import AppLogo from "./app-logo";
+import AppLogo from "./logo";
+import HeaderClientLink from "./link";
 import ShieldOption from "./shield-option";
 import ThemeToggler from "./theme-toggler";
 import StatusOptions from "./status-options";
@@ -9,20 +10,14 @@ const Header = () => {
     <div className="navbar fixed z-20 border-b border-b-base-300 bg-base-100 flex items-center justify-between">
       <div className="header-left flex items-center">
         <AppLogo />
-
-        <div className="group px-2 py-[5px] border border-solid border-[#e3e9f4] rounded-md ml-3 cursor-pointer">
-          <span className="group-hover:underline">@hitunezofficial</span>
-        </div>
+        <HeaderClientLink />
       </div>
 
       <div className="header-right">
         <div className="flex items-center">
           <ShieldOption />
-
           <ThemeToggler />
-
           <StatusOptions />
-
           <ProfileDropdown />
         </div>
       </div>
---
+++ src/components/header/link.tsx
@@ -0,0 +1,17 @@
+const HeaderClientLink = () => {
+  return (
+    <div className="pl-3">
+      <div className="breadcrumbs text-base">
+        <ul className="rounded-md border border-base-300 bg-base-100 px-2 py-[.32rem]">
+          <li className="w-15 truncate md:w-auto">
+            <a href="https://m.me/hitunezofficial" target="_blank" rel="noreferrer">
+              @hitunezofficial
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default HeaderClientLink;
